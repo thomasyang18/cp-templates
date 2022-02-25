71d9b5aefd3aed9183797c0871f04fd04976abb2
thomasyang18 <47704115+thomasyang18@users.noreply.github.com>
2022-02-24 23:31:34 -0500

    Update and rename segtrees to segtrees.cpp
    I DONT KNOW WHATS GOING ON

--- segtrees.cpp
+++ segtrees.cpp
@@ -40,33 +40,26 @@ struct lseg{
         n = 1;
         while (n < len) n*=2;
         t = vector<T>(n*2);
-        lazy = vector<L>(n*2);
-        for (int i = 0; i < n*2; i++) {
-            t[i] = idT;
-            lazy[i] = idL;
-        }
+        lazy = vector<L>(n);
+        for (int i = 0; i < n*2; i++) t[i] = idT, lazy[i/2] = idL;
     }
 
-    void build(ll p){ // build a single node
-        while (p > 1){
-            p/=2;
-            t[p] = combT(t[p*2], t[p*2+1]);
-        }
+    void apply(ll v, L value){
+        t[v] = applyL(t[v], value);
+        if (v < n) lazy[v] = combL(lazy[v], value);
     }
 
-    void parPushDown(ll p){
-        for (ll h = 26; h > 0; h--) {
-            ll i = p>>h; if (i > 0) push(i);
-        }
+    void build(ll p){
+        while(p /= 2) t[p] = applyL(combT(t[2 * p], t[2 * p + 1]) , lazy[p]);
     }
 
-    void push(ll v){
-        t[v] = applyL(t[v], lazy[v]);
-        if (v < n){
-            lazy[v*2] = combL(lazy[v*2], lazy[v]);
-            lazy[v*2+1] = combL(lazy[v*2+1], lazy[v]);
+    void push(ll p){
+        for (ll h = 26; --h;) { // never hits zero so don't worry about leaf node bs
+            ll v = p>>h;
+            apply(2*v, lazy[v]);
+            apply(2*v+1, lazy[v]);
+            lazy[v] = idL;
         }
-        lazy[v] = idL;
     }
 
     void update(ll l, ll r, L value) {
@@ -77,45 +70,100 @@ struct lseg{
         ll l0 = l, r0 = r;
         for (; l < r; l/=2, r/=2){
             if (l&1){
-                lazy[l] = combL(lazy[l], value);
-                push(l++);
+                apply(l++, value);
             }
             if (r&1){
-                lazy[--r] = combL(lazy[r], value);
-                push(r);
+                apply(--r, value);
             }
         }
+        // We call this because this range encapsulates all the updates we could possibly do on this range.
         build(l0), build(r0-1);
     }
 
     T query(ll l, ll r) { // fold f on interval [l, r)
         T resl=idT, resr=idT;
         l+=n, r+=n;
-        parPushDown(l), parPushDown(r-1);
+        // For a similar reason as build0, we push down all the updates to the nodes we could possibly visit.
+        push(l), push(r-1);
         for (; l < r; l /= 2, r /= 2) {
-            push(l); push(r);
             if (l&1) {
                 resl = combT(resl, t[l++]);
             }
             if (r&1) {
-                resr = combT(t[r--], resr);
+                resr = combT(t[--r], resr);
             }
         }
         return combT(resl, resr);
     }
 };
+#define NN 1048576
+namespace lseg2 {
+    typedef ll T;
+    typedef ll D;
 
+    T idT = 0, t[2 * NN];
+    D idD = 0, d[NN*2];
+    ll n = NN;
+
+    T f(T a, T b) { return a + b; }
+    D g(D a, D b) { return a+b; }
+    T h(D a, T b) {return a + b; }
+
+    void apply(ll p, D v) {
+        t[p] = h(v, t[p]);
+        if(p < n) d[p] = g(v, d[p]);
+    }
+
+    void build(ll p) {
+        while(p /= 2) t[p] = h(d[p], f(t[2 * p], t[2 * p + 1]));
+    }
+
+    void modify(ll l, ll r, D v) {
+        ll l0 = (l += n), r0 = (r += n);
+        for(; l < r; l /= 2, r /= 2) {
+            if(l & 1) apply(l++, v);
+            if(r & 1) apply(--r, v);
+        }
+        build(l0), build(r0-1);
+    }
+
+    void push(ll p) {
+        for(ll s=26; --s; ) {
+            ll i = p >> s;
+            apply(2 * i, d[i]);
+            apply(2 * i + 1, d[i]);
+            d[i] = idD;
+        }
+    }
+
+    T query(ll l, ll r) {
+        l += n, r += n;
+        push(l), push(r - 1);
+        T resL = idT, resR = idT;
+        for(; l < r; l /= 2, r /= 2) {
+            if(l & 1) resL = f(resL, t[l++]);
+            if(r & 1) resR = f(t[--r], resR);
+        }
+        return f(resL, resR);
+    }
+}
 
 void program1(){
     srand(30);
 #define N 10
 #define M 10
     ll a[N];
+    for (int i = 0; i < NN*2; i++){
+        lseg2::d[i] = lseg2::idD;
+        lseg2::t[i] = lseg2::idT;
+
+    }
     lseg *seg = new lseg(N);
     for (int i = 0; i < N; i++){
         ll x = rand()%M;
         a[i] = x;
-        seg->update(i,i+1,a[i]);
+       // seg->update(i,i+1,a[i]);
+       lseg2::modify(i, i+1, a[i]);
     }
     for (int t = 0; t < N; t++){
         ll i = rand()%N;
@@ -124,18 +172,25 @@ void program1(){
             if (i > j) swap(i,j);
             ll x = rand()%M;
             for (int l = i; l <= j; l++) a[l] = x;
-            seg->update(i, j+1, x);
+            //seg->update(i, j+1, x);
+            lseg2::modify(i, j+1, x);
         }
         else{
             ll ans = a[i];
+
             cout << i << endl;
             for (int l = 0; l < N; l++) {
                 if (i == l) cout << "," << a[l] << ", ";
                 else cout << a[l] << " ";
             }
             cout << endl;
-            cout << ans << " " << seg->query(i,i+1) << endl;
-            if (ans != seg->query(i, i+1)) {
+
+            // ll other = seg->query(i, i+1);
+            ll other = lseg2::query(i, i+1);
+
+            cout << ans << " " << other << endl;
+
+            if (ans != other) {
                 cout << "Nope\n";
                 return;
             }
