d6b63cd24da01f6fe58e3dd5b2abc3b273e0a6af
thomasyang18 <47704115+thomasyang18@users.noreply.github.com>
2022-03-02 16:09:32 -0500

    Create lseg.cpp

---
+++ lseg.cpp
@@ -0,0 +1,52 @@
+// combine joe and akif template, min segtree with addition atm
+namespace lseg {
+    typedef ll T;
+    typedef ll D;
+
+    T idT = 1e18, t[2 * NN];
+    D idD = 0, d[NN];
+    ll n = (fill_n(d, n, idD), NN);
+
+    T f(T a, T b) { return min(a,b); }
+    T g(T a, D b) { return a + b; }
+    D h(D a, D b) { return a + b; }
+
+    void apply(ll p, D v) {
+        t[p] = g(t[p], v);
+        if(p < n) d[p] = h(d[p], v);
+    }
+
+    void push(ll p) {
+        for(ll s=26; --s; ) {
+            ll i = p >> s;
+            apply(2 * i, d[i]);
+            apply(2 * i + 1, d[i]);
+            d[i] = idD;
+        }
+    }
+
+    void modifyP(ll p, T v = idT) {
+        if(p < n) { push(p += n); t[p] = v; }
+        while(p /= 2) t[p] = g(f(t[2 * p], t[2 * p + 1]), d[p]);
+    }
+
+    void modifyR(ll l, ll r, D v) {
+        ll l0 = (l += n), r0 = (r += n);
+        for(; l < r; l /= 2, r /= 2) {
+            if(l & 1) apply(l++, v);
+            if(r & 1) apply(--r, v);
+        }
+        modifyP(l0), modifyP(r0 - 1);
+    }
+
+    T query(ll l, ll r) {
+        l += n, r += n;
+        push(l), push(r - 1);
+        T resL = idT, resR = idT;
+        for(; l < r; l /= 2, r /= 2) {
+            if(l & 1) resL = f(resL, t[l++]);
+            if(r & 1) resR = f(t[--r], resR);
+        }
+        return f(resL, resR);
+    }
+}
