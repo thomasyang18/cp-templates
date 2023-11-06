329a53cec1c3ad7b8bc79d668ec7b4a46c514e08
thomasyang18 <47704115+thomasyang18@users.noreply.github.com>
2023-11-06 14:45:07 -0500

    Update combo_nt.cpp

--- combo_nt.cpp
+++ combo_nt.cpp
@@ -9,6 +9,31 @@ constexpr ll bpow(ll b, ll e) {
 constexpr ll inv(ll b) {
 	return bpow(b, M-2);
 }
+
+// large n, small k
+ll ncr(ll n, ll k) {
+    if (min(n, k) < 0) return 0;
+    if (n < k) return 0;
+    if (n == k || k == 0) return 1;
+
+    ll ans = 1;
+    F(i, 1, k+1) ans = ans * inv(i)%M;
+    F(i, n-k+1, n+1) ans = ans*(i%M)%M;
+    return ans;
+}
+
+ll ncr(ll n, ll k) {
+    if (min(n, k) < 0) return 0;
+    if (n < k) return 0;
+    if (n == k || k == 0) return 1;
+return f[i] * fi[n-k]%M * fi[k]%M;
+}
+
+ll sab(ll n, ll k) {
+    return ncr(n+k-1, k-1);
+}
+
+
 	f[0] = 1;
 	F(i, 1, NN) f[i] = f[i-1]*i%M;
 	F(i, 0, NN) fi[i] = inv(f[i]);
